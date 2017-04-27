4810b0fcf3434fb67c932b5de9b08f15a1ed3e27
jpramming <johanpramming@outlook.com>
2017-04-27 23:38:29 +0200

    set some ssh crypt options and close ssh connections

--- bigssh/bigssh.js
+++ bigssh/bigssh.js
@@ -52,8 +52,15 @@ module.exports = function(RED) {
         username: this.credentials.username,
         privateKey: undefined,
         privateKeyFile: this.credentials.privateKey,
-        passphrase: this.credentials.passphrase
-      }
+        passphrase: this.credentials.passphrase,
+		algorithms: {
+          kex: ['ecdh-sha2-nistp256','ecdh-sha2-nistp384','ecdh-sha2-nistp521','diffie-hellman-group-exchange-sha256','diffie-hellman-group14-sha1','diffie-hellman-group-exchange-sha1','diffie-hellman-group1-sha1'],
+          cipher: ['aes128-ctr','aes192-ctr','aes256-ctr','aes128-gcm','aes128-gcm@openssh.com','aes256-gcm','aes256-gcm@openssh.com','aes256-cbc','aes192-cbc','aes128-cbc','blowfish-cbc','3des-cbc','arcfour256','arcfour128','cast128-cbc','arcfour'],
+          serverHostKey: ['ssh-rsa','ssh-dss'],
+          hmac: ['hmac-sha2-256','hmac-sha2-512','hmac-sha1','hmac-md5','hmac-sha2-256-96','hmac-sha2-512-96','hmac-ripemd160','hmac-sha1-96','hmac-md5-96'],
+          compress: ['none','zlib@openssh.com','zlib']
+		}
+	  }
 
       //
       // In order to make this an instance while the object is common, it must not have any access to this.
@@ -100,10 +107,14 @@ module.exports = function(RED) {
 
         // this means "biglib"
         this.working("Connecting to " + ssh_config.host + "...");
+		
+		conn.on('error', function(err){
+			  conn.end()
+		});
 
         conn.on('ready', function() {
 
-          var commandLine = my_config.commandLine + ' ' + biglib.argument_to_string(my_config.commandArgs.concat(my_config.commandArgs2||[]));
+          var commandLine = my_config.commandLine + ' ' + biglib.argument_to_string(my_config.commandArgs.concat(my_config.commandArgs2));
 
           // this means "biglib" instance. Substr is to avoid a text too long
           this.working("Executing " + commandLine.substr(0,20) + "...");
@@ -118,10 +129,12 @@ module.exports = function(RED) {
 
                 // Gives biglib extra informations using the "stats" function                        
                 this.stats({ rc: code, signal: signal }); 
-                ret.emit('my_finish');         
+                ret.emit('my_finish');
+				conn.end();
               }.bind(this))
               .on('error', function(err) {
                 ret.emit('error', err);
+				conn.end();
               })
 
             // SSH stream is available, connect the bufstream
@@ -133,7 +146,7 @@ module.exports = function(RED) {
           }.bind(this));      
 
         }.bind(this))
-        .on('error', function(err) {
+         .on('error', function(err) {
           ret.emit('error', err);
         })
         .connect(ssh_config)
